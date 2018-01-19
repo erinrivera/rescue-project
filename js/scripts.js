f0968ef2ada081355bb1d7d3579e5697391c1e82
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-01-19 17:39:12 -0500

    created pet search function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
+var apiKey = "925dcf328934c0d0fa7102b11a2b93b0";
+var url = 'http://api.petfinder.com/shelter.getPets';
 
+$.ajax({
+    url: url,
+    jsonp: "jsonp",
+    data: {
+        key: apiKey,
+        id: 'KY30',
+        format: 'json'
+    },
+    success: function( response ) {
+        console.log(response);
+    }
+});
