08b080cf1d71df8b69b3b17f2309313819ac8677
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-01-22 12:16:58 -0500

    created ajax functions to get pet data from api

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 function petDisplay() {
+    // Variables defined
     var apiKey = "925dcf328934c0d0fa7102b11a2b93b0";
     var url = 'http://api.petfinder.com/shelter.getPets';
-
+    //AJAX Call to get pet data
     $.ajax({
         url: url,
-        jsonp: "jsonp",
+        jsonp: "callback",
+        dataType: "jsonp",
         data: {
             key: apiKey,
             id: 'KY30',
             format: 'json'
         },
         success: function( response ) {
-            console.log(response);
+            var data = [];
+            data = response.petfinder.pets.pet.[ ; 
+            console.log(data)
         }
     })
 };
