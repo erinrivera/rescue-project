ecb037c7f35fb547ed71e546882c6fe0a0a8e057
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-02-05 12:40:47 -0500

    added modal to pet-display page

--- js/scripts.js
+++ js/scripts.js
@@ -18,4 +18,39 @@ function petDisplay() {
             console.log(data)
         }
     })
+}
+
+function shortDesc(str) {
+    return str.split(/\s+/).slice(0,10).join(" ");
+}
+
+// Count photo array and create photos for modal slideshow
+function photoArray() {
+    var photoList = [];
+    if (picArray.length < 6) {
+        var photo1 = photoUrl + pfid + "/1/";
+        photoList.push(photo1);
+    } else if (picArray.length < 11) {
+        var photo2 = photoUrl + pfid + "/2/";
+        photoList.push(photo2);
+    } else if (picArray.length < 16) {
+        var photo3 = photoUrl + pfid + "/3/";
+        photoList.push(photo3);
+    } else if (picArray.length < 21) {
+        var photo4 = photoUrl + pfid + "/4/";
+        photoList.push(photo4);
+    } else if (picArray.length < 26) {
+        var photo4 = photoUrl + pfid + "/5/";
+        photoList.push(photo5);
+    } else if (picArray.length < 31) {
+        var photo5 = photoUrl + pfid + "/6/";
+        photoList.push(photo6);
+    }
+
+    // Create slideshow code for each picture
+    for (var i = 0; i < photoList.length; i++) {
+        var picCode = "";
+        ssCode += picCode;
+        return ssCode;
+    }
 };
