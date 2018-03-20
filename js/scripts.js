9ebb23e865c51b58ecb35fa6e555f087a8c434a0
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-03-19 21:43:24 -0400

    changed AJAX request to HTTPS

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 function petDisplay() {
     // Variables defined
     var apiKey = "925dcf328934c0d0fa7102b11a2b93b0";
-    var url = 'http://api.petfinder.com/shelter.getPets';
+    var url = 'https://api.petfinder.com/shelter.getPets';
     //AJAX Call to get pet data
     $.ajax({
         url: url,
