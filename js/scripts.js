0f75551f692be307c9fb9297a330cd79d7f466df
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-03-22 10:55:38 -0400

    added back to top button on pet dispay page

--- js/scripts.js
+++ js/scripts.js
@@ -30,6 +30,25 @@ function longDesc(str) {
     return str.split(/\s+/).slice(0,100).join(" ");
 }
 
+// BACK TO TOP BUTTON CODE
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("backToTopBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 // ABOUT PAGE
 
 //Accordion
